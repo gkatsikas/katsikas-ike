37b2f0f1c51f8a6bfbefe46f0d2e7ca1c6d4fc03
Georgios P. Katsikas <gkatsikas@ubitech.eu>
2026-01-03 03:30:18 +0200

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/katsikas-ike/",
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
