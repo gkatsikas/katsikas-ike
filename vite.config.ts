3d9699e43c067414a503ddb140838b8625a6d2bd
Georgios P. Katsikas <gkatsikas@ubitech.eu>
2026-01-03 03:11:07 +0200

    fix: vite workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/katsikas-ike/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  // server: {
+  //   host: "::",
+  //   port: 8080,
+  // },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
